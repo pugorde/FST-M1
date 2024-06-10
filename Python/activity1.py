f44ad93d2a0818af77406b64d1a6e6f1724a0b40
pugorde <132642715+pugorde@users.noreply.github.com>
2024-06-10 23:17:51 +0530

    Add files via upload

---
+++ Python/activity1.py
@@ -0,0 +1,9 @@
+# -------------Activity1---------
+username = input("Enter username:")
+print("username is:" + username
+      )
+age = input("Enter age:")
+print("Age is: "+ age)
+
+year = str ((2024 - int(age)) +100)
+print(username + " will be 100 years old in the year " + year)
